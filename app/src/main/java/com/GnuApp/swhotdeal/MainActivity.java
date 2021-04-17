8aebc9d22622642c6bed549d0dd2cc8f25aa86f2
kuhanaria <ssw0313@naver.com>
2021-04-17 10:33:14 +0900

    메인화면 디자인

--- app/src/main/java/com/GnuApp/swhotdeal/MainActivity.java
+++ app/src/main/java/com/GnuApp/swhotdeal/MainActivity.java
@@ -1,14 +1,60 @@
 package com.GnuApp.swhotdeal;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.GnuApp.swhotdeal.ui.hotdeal.HotdealFragment;
+import com.GnuApp.swhotdeal.ui.search.SearchFragment;
+import com.GnuApp.swhotdeal.ui.setting.SettingFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView maintitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavi);
+
+        getSupportFragmentManager().beginTransaction().add(R.id.main_frame,new HotdealFragment()).commit();
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                switch(menuItem.getItemId()){
+                    case R.id.navigation_hotdeal:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new HotdealFragment()).commit();
+                        break;
+                    case R.id.navigation_search:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new SearchFragment()).commit();
+                        break;
+                    case R.id.navigation_setting:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new SettingFragment()).commit();
+                        break;
+                }
+                return true;
+            }
+        });
+        /*AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_hotdeal, R.id.navigation_search, R.id.navigation_setting)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
+*/
     }
 }
