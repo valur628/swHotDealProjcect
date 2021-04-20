148d99d0b04e325a0e899d48932754e693a1d5fa
SH22Hwang <70620484+SH22Hwang@users.noreply.github.com>
2021-04-20 16:03:25 +0900

    지금까지 복원한 것 SplashActivity, MainActivity, SearchActivity esdlogo, color, layout recyclerView data, layout

--- app/src/main/java/com/GnuApp/swhotdeal/MainActivity.java
+++ app/src/main/java/com/GnuApp/swhotdeal/MainActivity.java
@@ -7,26 +7,19 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.view.View;
 
+import com.GnuApp.swhotdeal.SearchActivity;
+
 public class MainActivity extends AppCompatActivity {
     public static final int REQUEST_CODE_MENU = 101;
+    String TAG = "MainActivity";
+    Button searchButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        Button button = findViewById(R.id.button_search);
-//        button.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent();
-//                intent.putExtra("ddd", "mike");
-//                setResult(RESULT_OK, intent);
-//                finish();
-//            }
-//        });
-
-        Button searchButton = findViewById(R.id.button_search);
+        searchButton = findViewById(R.id.button_search);
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
