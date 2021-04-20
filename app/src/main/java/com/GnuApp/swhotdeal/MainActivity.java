53f37f24555a04cd83218786ed3a6c553e50bb1c
SH22Hwang <70620484+SH22Hwang@users.noreply.github.com>
2021-04-20 17:18:50 +0900

    원인 발견, recycler view를 만들었는데 어떻게 다룰지 안 정함.

--- app/src/main/java/com/GnuApp/swhotdeal/MainActivity.java
+++ app/src/main/java/com/GnuApp/swhotdeal/MainActivity.java
@@ -1,7 +1,10 @@
 package com.GnuApp.swhotdeal;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -10,23 +13,34 @@ import android.view.View;
 
 import com.GnuApp.swhotdeal.SearchActivity;
 
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
     public static final int REQUEST_CODE_MENU = 101;
-    String TAG = "MainActivity";
+
+    Activity activity;
     Button searchButton;
+    ArrayList<HotDeal> hotDealArrayList;
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-//        Handler handler = new Handler();
-//        handler.postDelayed(new Runnable() {
-//            public void run() {
-//                setContentView(R.layout.activity_main); // theme로 스플래시 화면 구현
-//            }
-//        }, 2000);
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String TAG = "MainActivity";
+        String beforeString = "";
+
+        activity = this;
+        hotDealArrayList = new ArrayList<>();
+        layoutManager = new LinearLayoutManager(activity);
+        recyclerView = findViewById(R.id.hotdeal_recycler);
+//        recyclerView.setHasFixedSize(true);
+//        recyclerView.setLayoutManager(layoutManager);
+        // recyclerView를 만들긴 했는데 어떻게 다룰지 안 만듦.
+        // recyclerView를 다루기 위한 어댑터를 만들자!
 
         searchButton = findViewById(R.id.button_search);
         searchButton.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/GnuApp/swhotdeal/SearchActivity.java
+++ app/src/main/java/com/GnuApp/swhotdeal/SearchActivity.java
@@ -1,28 +1,43 @@
 package com.GnuApp.swhotdeal;
 
+import android.app.Activity;
 import android.app.SearchManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.SearchView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
 public class SearchActivity extends AppCompatActivity {
 
+    Activity activity;
+    ArrayList<HotDeal> hotDealArrayList;
     RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
     SearchView searchView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        String TAG = "SearchActivity";
+        String beforeString = "";
 
+        activity = this;
+        hotDealArrayList = new ArrayList<>();
+        layoutManager = new LinearLayoutManager(activity);
         recyclerView = (RecyclerView) findViewById(R.id.hotdeal_recycler);
         searchView = (SearchView) findViewById(R.id.search_view);
+//        recyclerView.setHasFixedSize(true);
+//        recyclerView.setLayoutManager(layoutManager);
+        // recyclerView를 만들긴 했는데 어떻게 다룰지 안 만듦.
+        // recyclerView를 다루기 위한 어댑터를 만들자!
 
         // Get the intent, verify the action and get the query
         Intent intentSearch = getIntent();
